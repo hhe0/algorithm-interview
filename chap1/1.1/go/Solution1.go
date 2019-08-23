5ee01557e51ce9e52db4170f10a3c7ad8b889b65
hhe0 <deng2296917779@163.com>
2019-08-23 16:56:43 +0800

    1.1

--- chap1/1.1/go/Solution1.go
+++ chap1/1.1/go/Solution1.go
@@ -8,4 +8,19 @@ func main() {
 	sliceData := slice.SliceType{1, 2, 3, 4, 5, 6}
 	head := sliceData.Slice2LinkList()
 	head.PrintLinkList()
+
+	// 转 slice 处理
+	tempNode := head.Next
+	sliceLinkList := slice.SliceType{}
+	for tempNode != nil {
+		sliceLinkList = append(sliceLinkList, tempNode.Data)
+		tempNode = tempNode.Next
+	}
+
+	// 反转 slice 元素的顺序
+	sliceLinkList = sliceLinkList.ReverseSlice()
+
+	// 转 linklist
+	linkList := sliceLinkList.Slice2LinkList()
+	linkList.PrintLinkList()
 }
--- structure/slice/slice.go
+++ structure/slice/slice.go
@@ -1,6 +1,9 @@
 package slice
 
-import "algorithm-interview/structure/linklist"
+import (
+	"algorithm-interview/structure/linklist"
+	"fmt"
+)
 
 type SliceType []interface{}
 
@@ -29,4 +32,21 @@ func (s SliceType) Slice2LinkList() *linklist.LinkNode {
 	}
 
 	return head
+}
+
+// 反转切片
+func (s SliceType) ReverseSlice() SliceType {
+	temp := s[0]
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		temp = s[i]
+		s[i] = s[j]
+		s[j] = temp
+	}
+
+	return s
+}
+
+// 打印切片
+func (s SliceType) PrintSlice() {
+	fmt.Println(s)
 }
